44c83b6027e33da74ac0e1013b3eeb72e7984593
Damián Muszalski <dmuszalski@gmail.com>
2015-12-02 14:54:29 -0300

    Limpié un poco los comentarios.

--- PreGoApp/web/www/serviceCreate_service.js
+++ PreGoApp/web/www/serviceCreate_service.js
@@ -2,13 +2,6 @@ app.factory('PartyServicesService', function($http, $q) {
 	return {
 
 		getPublishedServices : function() {
-//			return simpleHttpGet($http, '/api/services', $q.reject);
-//			return "Garompa";
-//			$http.get("/api/services").success(function (data) {
-//				$scope.friends = data;
-//			}).error(function () {
-//				alert("an unexpected error ocurred!");
-//			});
 			return simpleHttpGet($http, '/api/services', $q.reject);
 		}
 	}

17ef8f40acb4f7dfa9bfe1893798a5cf6ca57d2a
nahuelgq <nahuelquevedo@gmail.com>
2015-12-13 02:57:56 -0300

    Pruebas: separadas las pruebas en archivos diferentes. Se corren igual que antes, mocha recorre todos los js

--- PreGoApp/test/encuentrosServicesTests.js
+++ PreGoApp/test/encuentrosServicesTests.js
@@ -1,84 +1,7 @@
 var assert = require('chai').assert;
 var PregoServices = require('../prego_services.js');
 
-
-
-describe('PregoServices', function() {
-  describe('#Usuarios', function () {
-	var createUsuariosService = function () { 
-		var usuariosService = new PregoServices().getUsuariosService();
-		return usuariosService;
-	}
-
-    
-	it('deberia devolver true', function () {
-	    var usuarios = createUsuariosService();
-
-		assert.equal(true, usuarios.agregarUsuario('pepe@pepe.com','pepePass').exito);
-    });	
-	
-	it('no deberian haber usuarios al principio', function () {
-	    var usuarios = createUsuariosService();
-
-		assert.equal(0, usuarios.getUsuarios().length);
-    });
-	
-	it('deberia existir el usuario recien agregado', function () {
-	    var usuarios = createUsuariosService();
-		
-		usuarios.agregarUsuario('pepe@pepe.com','pepePass');
-		assert.equal('pepe@pepe.com', usuarios.getUsuarios()[0].email);
-    });
-	
-	it('no deberia permitir usuarios duplicados', function () {
-	    var usuarios = createUsuariosService();
-		
-		usuarios.agregarUsuario('usuarioDup','pepePass');
-		
-		assert.equal(false, usuarios.agregarUsuario('usuarioDup','pepePass').exito);
-		assert.equal(1, usuarios.getUsuarios().length);
-    });
-	
-
-	it('deberia permitir loggearse con credenciales correctas', function () {
-	    var usuarios = createUsuariosService();
-		
-		usuarios.agregarUsuario('pepe@pepe.com','pepePass');
-		
-		assert.equal(true, usuarios.login('pepe@pepe.com','pepePass'));
-    });
-	
-	it('no deberia permitir loggearse con credenciales erroneas', function () {
-	    var usuarios = createUsuariosService();
-		
-		usuarios.agregarUsuario('pepe@pepe.com','pepa');
-		
-		assert.equal(false, usuarios.login('pepe@pepe.com','pepe@pepe.com'));
-    });
-	
-	
-	it('deberian permitir rellenar la base con datos de prueba', function () {
-	    var usuarios = createUsuariosService();
-		
-		usuarios.rellenar();
-		
-		assert.equal(true, usuarios.getUsuarios().length>=6);
-    });
-	
-	
-	it('deberian permitir buscar un usuario por email', function () {
-	    var usuarios = createUsuariosService();
-		
-		usuarios.rellenar();
-		
-		assert.equal("Nahuel", usuarios.getUsuarioByEmail("nahuel@prego.com").nickname);
-		assert.equal("M", usuarios.getUsuarioByEmail("nahuel@prego.com").sexo);
-    });
-	
-	
-  });
-  
-  
+describe('PregoServices', function() { 
   
   describe('#Encuentros', function () {
 	  
@@ -270,4 +193,8 @@ describe('PregoServices', function() {
 	//deberia traer primero los usuarios de fiestas a las que va el usuarioConocedor	
 	
   });
+  
+  
+  
+  
 });
---
+++ PreGoApp/test/partyServicesTests.js
@@ -0,0 +1,14 @@
+var assert = require('chai').assert;
+var PregoServices = require('../prego_services.js');
+
+describe('PregoServices', function() {
+  describe('#Usuarios', function () {
+	 
+	it('deberia devolver true', function () {
+	     
+		assert.equal(true, 1);
+    });	
+	 
+	
+  });
+});

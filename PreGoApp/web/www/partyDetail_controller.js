84f507a899d137541b2c1774b162c4e036392c73
daniel321 <obi.arts@gmail.com>
2015-12-13 22:23:09 -0300

    mapa en pag de fiestas

--- PreGoApp/web/www/partyDetail_controller.js
+++ PreGoApp/web/www/partyDetail_controller.js
@@ -1,31 +1,144 @@
 app.controller('partyDetailController', function ($scope, $http, $location, PartyDetailService) {
-    $scope.position = [];
+	$scope.position = [0,0];
+	$scope.positionDest = [0,0];
 
-    function success_callback(p) {
-        $scope.position[0] = p.coords.latitude;
-        $scope.position[1] = p.coords.longitude;
-        getPartyDistance();
-    }
+	$scope.initialized = false;
 
-    function error_callback(p) {
-        $scope.position[0] = -34.617568;
-        $scope.position[1] = -58.368352;
-        getPartyDistance();
-        console.log('error=' + p.message);
-    }
+	function initialize_map(id){
+		var myOptions = {
+			zoom: 4,
+			mapTypeControl: true,
+			mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
+			navigationControl: true,
+			navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
+			mapTypeId: google.maps.MapTypeId.ROADMAP      
+		}	
+		map = new google.maps.Map(document.getElementById(id), myOptions);
+	}
 
-    function getPos() {
-        if ($scope.initialized || geo_position_js.init()) {
-            geo_position_js.getCurrentPosition(success_callback, error_callback, { enableHighAccuracy: true });
-            $scope.initialized = true;
-        } else {
-            $scope.position[0] = -34.617568;
-            $scope.position[1] = -58.368352;
+	function initialize(){
+		if(geo_position_js.init())
+		{
+			document.getElementById('current').innerHTML="Receiving...";
+			geo_position_js.getCurrentPosition(show_position,function(){document.getElementById('current').innerHTML="Couldn't get location"},{enableHighAccuracy:true});
+		}
+		else
+		{
+			document.getElementById('current').innerHTML="Functionality not available";
+		}
+	}
 
-            console.log("Functionality not available");
-        }
-    }
-    getPos();
+	function show_position(p){
+		$scope.position[0] = p.coords.latitude;
+		$scope.position[1] = p.coords.longitude;
+
+		document.getElementById('current').innerHTML="latitude="+$scope.position[0].toFixed(5)+" longitude="+$scope.position[1].toFixed(5);
+		var pos=new google.maps.LatLng($scope.position[0],$scope.position[1]);
+		var pos2=new google.maps.LatLng($scope.positionDest[0],$scope.positionDest[1]);
+
+		var bounds = new google.maps.LatLngBounds();
+		bounds.extend(pos);
+		bounds.extend(pos2);
+		map.fitBounds(bounds);
+		// map.setCenter(pos);
+		//map.setZoom(14);
+
+		var dirDisp = new google.maps.DirectionsRenderer();
+		dirDisp.setMap(map);
+		dirDisp.setOptions(
+			{
+				polylineOptions:{strokeColor:"#4a4a4a",strokeWeight:3}, 
+				suppressMarkers: true
+			}
+		);
+
+		var dirSvc = new google.maps.DirectionsService();
+		var req = {
+			origin:pos,
+			destination: pos2,
+			travelMode: google.maps.TravelMode.WALKING
+//			travelMode: google.maps.TravelMode.DRIVING
+		};
+		dirSvc.route(
+			req,
+			function (response,status){
+				if(status == google.maps.DirectionsStatus.OK){
+					dirDisp.setDirections(response);
+					dirDisp.setMap(map);
+				}				
+			}
+		);
+
+   		var pinShadow = new google.maps.MarkerImage(
+			"http://chart.apis.google.com/chart?chst=d_map_pin_shadow",
+	        	new google.maps.Size(40, 37),
+	        	new google.maps.Point(0, 0),
+	        	new google.maps.Point(12, 35)
+		);
+
+	    	var pinColor = "FE7569";
+	    	var pinImage = new google.maps.MarkerImage(
+			"http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
+			new google.maps.Size(21, 34),
+			new google.maps.Point(0,0),
+			new google.maps.Point(10, 34)
+		);
+
+		var marker = new google.maps.Marker({
+		    position: pos,
+		    map: map,
+		    shadow:pinShadow,
+		    icon: pinImage,
+		    title:"You are here",
+		    draggable:false
+		});
+
+	    	var pinColor2 = "5314F3";
+	    	var pinImage2 = new google.maps.MarkerImage(
+			"http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor2,
+			new google.maps.Size(21, 34),
+			new google.maps.Point(0,0),
+			new google.maps.Point(10, 34)
+		);
+
+		var marker2 = new google.maps.Marker({
+		    position: pos2,
+		    map: map,
+		    shadow:pinShadow,
+		    icon: pinImage2,
+		    title:"You are going here",
+		    draggable:false
+		});
+
+	}
+
+	function success_callback(p){
+		$scope.position[0] = p.coords.latitude;
+		$scope.position[1] = p.coords.longitude;
+	}
+		
+	function error_callback(p){
+		console.log('error='+p.message);
+	}
+
+	var getPos = function(){
+		if( $scope.initialized || geo_position_js.init() ){
+			geo_position_js.getCurrentPosition(success_callback,error_callback,{enableHighAccuracy:true});
+			$scope.initialized = true;
+		}else{
+			console.log("Functionality not available");
+		}
+
+	}
+
+	getPos();
+
+	if (!($scope.mapInitialized)){
+		initialize_map("map_canvas2");
+		initialize();
+
+		$scope.mapInitialized = true;
+	}	
 
     var partyId = $location.search().id;
 	var partyName = $location.search().nombre;
@@ -43,7 +156,7 @@ app.controller('partyDetailController', function ($scope, $http, $location, Part
 
     $scope.musicGenres = [];
     $scope.selectedMusicGenres = [];
-
+	
     $scope.location = {
         name: null,
         lat: null,
@@ -122,6 +235,8 @@ app.controller('partyDetailController', function ($scope, $http, $location, Part
         PartyDetailService.getParty(partyKey)
             .then(function (response) {
                 $scope.party = response.data;
+    		$scope.positionDest = [$scope.party.pos.lat,$scope.party.pos.long];
+
                 getPartyFinished = true;
                 callback();
             })
